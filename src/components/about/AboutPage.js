c36f3bac82d5ffd203769d9bb090f9ee502cd084
urvaius <urvaius@gmail.com>
2018-03-15 13:34:04 -0700

    starting to build app

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class AboutPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>About</h1>
+                <p>This application uses react, redux, react router and a variety of other helpfull libraries</p>
+            </div>
+        );
+    }
+}
+
+export default AboutPage;
